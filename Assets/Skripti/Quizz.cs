872f146ed51e98e279e0c86e792e3468c52144f5
Nauris <naurissg@gmail.com>
2023-06-10 00:15:48 +0300

    Papildinats projekts ar answer option pievienot, pievienoti jauni image nomainits kursors un papildinats kods

---
+++ Assets/Skripti/Quizz.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quizz : MonoBehaviour
+{
+	public Question[] questions; // Array to store all the questions
+	public Text questionText; // Reference to the question text component
+	public Button[] answerButtons; // Array to store the answer buttons
+	public Image feedbackImage; // Reference to the image component for feedback
+
+	public Sprite correctImage; // Sprite for correct answer feedback
+	public Sprite incorrectImage; // Sprite for incorrect answer feedback
+
+	private int currentQuestionIndex = 0; // Index of the current question
+	private TimerSkripts timerScript; // Reference to the TimerSkripts script
+
+	private void Start()
+	{
+		timerScript = FindObjectOfType<TimerSkripts>(); // Find the TimerSkripts script in the scene
+		DisplayQuestion(currentQuestionIndex); // Display the first question
+	}
+
+	private void DisplayQuestion(int questionIndex)
+	{
+		// Update the question text
+		questionText.text = questions[questionIndex].question;
+
+		// Loop through the answer buttons and set their text
+		for (int i = 0; i < answerButtons.Length; i++)
+		{
+			answerButtons[i].GetComponentInChildren<Text>().text = questions[questionIndex].answers[i];
+		}
+	}
+
+	public void AnswerButtonClicked(int buttonIndex)
+	{
+		// Check if the selected answer is correct
+		if (questions[currentQuestionIndex].correctAnswerIndex == buttonIndex)
+		{
+			// Answer is correct
+			Debug.Log("Correct!");
+
+			// Show the correct answer image
+			ShowFeedbackImage(correctImage);
+		}
+		else
+		{
+			// Answer is wrong
+			Debug.Log("Wrong!");
+
+			// Show the incorrect answer image
+			ShowFeedbackImage(incorrectImage);
+		}
+
+		// Move to the next question
+		currentQuestionIndex++;
+
+		// Check if all questions have been answered
+		if (currentQuestionIndex >= questions.Length)
+		{
+			Debug.Log("Quiz completed!");
+
+			// Call the QuizCompleted method in TimerSkripts script to stop the timer
+			timerScript.QuizCompleted();
+
+			// Transition to a different scene (change "Beigas" to the name of your scene)
+			SceneManager.LoadScene("Beigas");
+			return;
+		}
+
+		// Display the next question after a delay
+		Invoke("DisplayNextQuestion", 2f);
+	}
+
+	private void DisplayNextQuestion()
+	{
+		// Hide the feedback image
+		feedbackImage.gameObject.SetActive(false);
+
+		// Display the next question
+		DisplayQuestion(currentQuestionIndex);
+	}
+
+	private void ShowFeedbackImage(Sprite image)
+	{
+		// Set the feedback image sprite
+		feedbackImage.sprite = image;
+
+		// Show the feedback image
+		feedbackImage.gameObject.SetActive(true);
+	}
+}
+
+[System.Serializable]
+public class Question
+{
+	public string question; // The question
+	public string[] answers; // Array of possible answers
+	public int correctAnswerIndex; // Index of the correct answer
+}
---
+++ Assets/Skripti/TimerSkripts.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerSkripts : MonoBehaviour
+{
+	public Text timerText; // Reference to the text component to display the timer
+	private float timer = 0f; // The timer value
+	private bool quizCompleted = false; // Flag to track if the quiz is completed
+
+	private void Update()
+	{
+		if (!quizCompleted)
+		{
+			// Update the timer value
+			timer += Time.deltaTime;
+
+			// Calculate minutes, seconds, and milliseconds
+			int minutes = Mathf.FloorToInt(timer / 60f);
+			int seconds = Mathf.FloorToInt(timer % 60f);
+			int milliseconds = Mathf.FloorToInt((timer * 1000) % 1000);
+
+			// Update the timer text
+			timerText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+		}
+	}
+
+	public void QuizCompleted()
+	{
+		quizCompleted = true;
+		SceneManager.LoadScene("Beigas"); // Transition to the "Beigas" scene (replace with your scene name)
+		PlayerPrefs.SetFloat("FinalTime", timer); // Store the final time in player preferences for accessing in the "Beigas" scene
+	}
+}
